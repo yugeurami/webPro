e8e885bac17d9c24e712b310544de05fe546ad62
yugeurami <kimyujin4696@gmail.com>
2022-04-29 16:19:33 +0900

    2022.04.29

--- source/01.java/main/src/main/Main.java
+++ source/01.java/main/src/main/Main.java
@@ -1,20 +1,31 @@
 package main;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class Main {
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		int su = scanner.nextInt();
-		for(int i = 0 ; i<su ; i++) {
-			for(int s = su-i ; s>0 ; s--) {
-				System.out.print(" ");
+		try {
+			InputStream in = System.in;
+			InputStreamReader ir = new InputStreamReader(in);
+			BufferedReader br = new BufferedReader(ir);
+			
+			int N = Integer.parseInt(br.readLine());
+			StringBuilder sb = new StringBuilder();
+			
+			for(int i = 0 ; i < N ; i++) {
+				String str = br.readLine();
+				int target = str.indexOf(" ");
+				int result = Integer.parseInt(str.substring(0, target))+Integer.parseInt(str.substring(target+1));
+				sb.append(result+"\n");
 			}
-			for(int j = 0 ; j <=i ; j++) {
-				System.out.print("*");
-			}
-			System.out.println();
+			br.close();
+			System.out.println(sb);
+			
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-		scanner.close();
 	}
 }
---
+++ source/04.HTML_CSS/ch01/2.js
@@ -0,0 +1,5 @@
+// 2.js
+name = prompt("니 이름이 뭐니?", "홍길동");
+if (name != 'null') {
+    document.write(name + "~ 방가방가<br>");
+}
