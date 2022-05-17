758e1313dfe69c8f27833eff0079746b373716a1
yugeurami <kimyujin4696@gmail.com>
2022-05-17 18:19:38 +0900

    2022.05.17

---
+++ source/05.javaScript/ch08_기본내장객체/intervalDay.js
@@ -0,0 +1,9 @@
+﻿Date.prototype.intervalDay = function (that) {
+    if (this > that) {
+        intervalMili = this.getTime() - that.getTime();
+    } else {
+        intervalMili = that.getTime() - this.getTime();
+    }
+    var day = intervalMili / (1000 * 60 * 60 * 24);
+    return Math.trunc(day);
+}
