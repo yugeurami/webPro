852e1bfdbfdda6b3734c1e7499458b640d5a13cd
yugeurami <kimyujin4696@gmail.com>
2022-06-16 10:15:14 +0900

    2022.06.16

---
+++ source/06.JSP/ch18/src/com/lec/frontController/FrontController.java
@@ -0,0 +1,36 @@
+package com.lec.frontController;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("*.do")
+public class FrontController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public FrontController() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		actionDo(request, response);
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		actionDo(request, response);
+	}
+
+	private void actionDo(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String uri = request.getRequestURI();// /ch18/*.do
+		String conPath = request.getContextPath(); // ch18/
+		String command = uri.substring(conPath.length());
+		System.out.println(uri);
+		System.out.println(conPath);
+		System.out.println(command);
+		
+	}
+}
